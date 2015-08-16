59ccdf74402271e38e7f893e22f63096762a6573
Sumeet <sumeet.ninawe@gmail.com>
2015-08-17 00:47:33 +0530

    17Aug Commit

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 var express 	= require('express');
+var cons 		= require('consolidate');
 var app 		= express();
 var admin 		= express();
 var secret 		= express();
 
+var users = [];
+users.push({ name: 'tobi'});
+users.push({ name: 'loki'});
+users.push({ name: 'jane'});
 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('view', __dirname + '/index');
 app.use(express.static('public'));
 app.use(['/adm*n'], admin);
 admin.use('/secr*t', secret);
 
 /*------app------*/
 app.get('/', function(req, res){
-	res.send('Hello world');
+	res.render('index', {
+		title: 'Consolidate.js'
+	});
+});
+app.get('/users', function(req, res){
+	res.render('users', {
+		title: 'Users',
+		users: users
+	});
 });
 app.delete('/', function(req, res){
-	res.send('DELETE Request on home page');
+	res.send('DELETE Request to home page');
 	console.log('DELETE received');
 });
 
